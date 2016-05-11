419e92fad4d67dcb7ffaceecde6a15237eeb12ca
tong <tong@disktree.net>
2016-05-11 14:23:09 +0200

    import

---
+++ web/panzerschokolade.js
@@ -0,0 +1,8 @@
+(function (console) { "use strict";
+var panzerschokolade_App = function() { };
+panzerschokolade_App.main = function() {
+	window.onload = function() {
+	};
+};
+panzerschokolade_App.main();
+})(typeof console != "undefined" ? console : {log:function(){}});
